9d718a469cb51e45bc5d30f95a3dfa90d71442f1
betojr98 <robertosouza.03@hotmail.com>
2017-06-11 16:03:46 -0300

    Add files via upload

---
+++ video_04.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include<windows.h>
+
+
+int main()
+
+{
+    char a[100],b[100],c[100],d[100],e[100],f[100],g[100],h[100],i[100],j[100];
+    int t;
+
+    printf("Como parar de procrastinar(adiar, deixar para o outro dia, inventar desculpas\n");
+    printf("------------------------------------------\n");
+    printf("1-Principio -- Escreva as tarefas que geram 80%% de resultado no seu dia\n");
+    printf("Para este exemplo, iremos utilizar 2 tarefas\n");
+
+    gets(a);
+    gets(b);
+
+    printf("------------------------------------------\n");
+    printf("Agora digite 8 tarefas que geram 20%% de resultado no seu dia\n");
+
+    gets(c);
+    gets(d);
+    gets(e);
+    gets(f);
+    gets(j);
+    gets(h);
+    gets(i);
+    gets(j);
+
+    printf("------------------------------------------\n");
+    printf("2-Principio -- Comece o dia com as tarefas mais importantes:(%s, %s)\n", a,b);
+
+    printf("------------------------------------------\n");
+
+    Sleep(4000);
+
+
+    printf("3-Principio -- Anote as ideias e Faca uma revisao.\nTer um caderno separado para anotar ideias e pensamentos.\n");
+    printf("Caso tenha ideias, pare tudo e anote imediatamente.\n");
+    printf("Uma vez na semana faca uma revisão.\nAgende os seus proximos 7 dias\n");
+    printf("------------------------------------------\n");
+
+     Sleep(7000);
+
+     printf("4-Principio -- Crie um senso de urgencia(estabeleça um senso de urgencia,\n determine um prazo)\n");
+     printf("Isso aumenta o comprometimento e os resultados.\n");
+     printf("------------------------------------------\n");
+
+     Sleep(5000);
+
+     printf("5-Principio -- Adicione recompensas para tarefas compridas e consequencia para as tarefas não feitas.\n");
+     printf("------------------------------------------\n");
+
+     Sleep(4000);
+
+     printf("6-Principio -- Comece aos poucos e vai aumentando gradativamente a cada semana\n Ate atingir os seus objetivos.\n");
+     printf("------------------------------------------\n");
+
+     Sleep(4000);
+
+     printf("7-Principio -- Monitore o seu progresso, Dica(Utilize um app de monitoramento de habito\.n");
+     printf("Liste Habitos que tem ou quer seguir e escolha horarios para realizalos.\n");
+
+
+
+
+    return 0;
+
+
+}
